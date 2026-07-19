070f87fd54a39ee6bcd380b25160cca0994006cb
KRITHIK KR <krithikkattyparambil@gmail.com>
2026-07-19 08:53:29 +0530

    Add files via upload

---
+++ krithikkattyparambil@gmail.com/week 5/handson5/src/data/courses.js
@@ -0,0 +1,7 @@
+export const initialCourses = [
+  { id: 1, name: 'Data Structures', code: 'CS101', credits: 4, grade: 'A' },
+  { id: 2, name: 'Web Development', code: 'CS102', credits: 3, grade: 'A-' },
+  { id: 3, name: 'Database Systems', code: 'CS201', credits: 4, grade: 'B+' },
+  { id: 4, name: 'Operating Systems', code: 'CS202', credits: 4, grade: 'B' },
+  { id: 5, name: 'Cloud Computing', code: 'CS301', credits: 3, grade: 'A' },
+];
